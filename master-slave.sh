3a5833ef378942d5ba4a3cf4ce31cbdd70e53022
shashi0396 <shashikirandc1315@gmail.com>
2026-07-24 09:21:42 +0530

    updated the master-slave jenkins script

--- master-slave.sh
+++ master-slave.sh
@@ -12,6 +12,7 @@ sudo adduser jenkins
 sudo usermod -aG sudo jenkins
 sudo mkdir -p /home/jenkins/.ssh
 
+
 #----------------------------------------------
 #On MASTER:
 # ----------------------------------------------
@@ -19,7 +20,7 @@ sudo mkdir -p /home/jenkins/.ssh
 sudo su - jenkins
 ssh-keygen
 #Copy Public Key from Master
-sudo cat /var/lib/jenkins/.ssh/id_ed25519.pub
+cat /var/lib/jenkins/.ssh/id_ed25519.pub
 
 
 #-------------------------------------
@@ -36,6 +37,10 @@ sudo chmod 600 /home/jenkins/.ssh/authorized_keys
 sudo su - jenkins
 ssh jenkins@172.31.36.26
 
+# --------------------------------------------------
+# Add the node in Jenkins UI with below details:
+# --------------------------------------------------
+
 Number of executors 2
 
 Remote root directory
@@ -46,14 +51,16 @@ linux
 
 Launch method
 launch agent via SSH
+
 Host
 172.31.33.28
+
 Credentials
 Add new credentials
 Kind
 SSH Username with private key
-Scope
-Global
+
+Scope - Global
 Id
 agent-key
 Description
@@ -65,5 +72,28 @@ Private Key
 sudo cat /var/lib/jenkins/.ssh/id_ed25519
 Passphrase
 Leave empty
+
 Host Key Verification Strategy
 Non verifying Verification Strategy
+
+
+
+# USERDATA SCRIPT FOR AGENT NODE
+
+sudo apt update -y
+sudo apt install fontconfig openjdk-21-jre -y
+java -version
+
+sudo adduser jenkins
+# Set password
+
+# Give Sudo Access
+sudo usermod -aG sudo jenkins
+sudo mkdir -p /home/jenkins/.ssh
+wget https://s3.amazonaws.com/s3/id_ed25519.pub
+
+sudo mv id_ed25519.pub /home/jenkins/.ssh/authorized_keys
+# Paste key.
+sudo chown -R jenkins:jenkins /home/jenkins/.ssh
+sudo chmod 700 /home/jenkins/.ssh
+sudo chmod 600 /home/jenkins/.ssh/authorized_keys
